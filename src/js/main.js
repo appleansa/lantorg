a822e7300e4a66126385c3a975df8daaffa0414c
Andrii Shevchyk <appleansa@gmail.com>
2020-10-16 21:08:00 +0300

    cart.html - new page

--- src/js/main.js
+++ src/js/main.js
@@ -460,7 +460,7 @@
                      spaceBetween: 0,
                      updateOnWindowResize: true,
                      pagination: {
-                         el: '.j-pc-branch-pagination-' + pcElem,
+                         el: '.pc-branch-pagination-'+pcElem,
                          clickable: true,
                      },
                      navigation: {
@@ -473,20 +473,6 @@
                  pcElem++;
 
              });
-
-
-
-
-             // console.log(elem);
-
-
-             // for ( var i = 1; i <= elems.length; i++ ) {
-
-             //   elems.eq(i).css({'order': i});
-
-             // }
-
-
 
 
              // micromodal init function
@@ -921,3 +907,98 @@
                  });
 
              })(); /* IIFE end */
+
+
+
+ 
+/// input number customization
+
+$('.quantity').each(function( ) {
+ 
+ 
+
+    var $quantityArrowMinus = $(this).find(".quantity-arrow-minus");
+    var $quantityArrowPlus = $(this).find(".quantity-arrow-plus");
+    var $quantityNum = $(this).find(".quantity-num");
+
+    $quantityArrowMinus.click(quantityMinus);
+    $quantityArrowPlus.click(quantityPlus);
+
+    function quantityMinus() {
+      if ($quantityNum.val() > 1) {
+        $quantityNum.val(+$quantityNum.val() - 1);
+      }
+    }
+
+    function quantityPlus() {
+      $quantityNum.val(+$quantityNum.val() + 1);
+    }
+  });
+
+
+/// inpur select customization
+
+// Iterate over each select element
+$('select').each(function () {
+
+    // Cache the number of options
+    var $this = $(this),
+        numberOfOptions = $(this).children('option').length;
+
+    // Hides the select element
+    $this.addClass('s-hidden');
+
+    // Wrap the select element in a div
+    $this.wrap('<div class="select"></div>');
+
+    // Insert a styled div to sit over the top of the hidden select element
+    $this.after('<div class="styledSelect"></div>');
+
+    // Cache the styled div
+    var $styledSelect = $this.next('div.styledSelect');
+
+    // Show the first select option in the styled div
+    $styledSelect.text($this.children('option').eq(0).text());
+
+    // Insert an unordered list after the styled div and also cache the list
+    var $list = $('<ul />', {
+        'class': 'options'
+    }).insertAfter($styledSelect);
+
+    // Insert a list item into the unordered list for each select option
+    for (var i = 0; i < numberOfOptions; i++) {
+        $('<li />', {
+            text: $this.children('option').eq(i).text(),
+            rel: $this.children('option').eq(i).val()
+        }).appendTo($list);
+    }
+
+    // Cache the list items
+    var $listItems = $list.children('li');
+
+    // Show the unordered list when the styled div is clicked (also hides it if the div is clicked again)
+    $styledSelect.click(function (e) {
+        e.stopPropagation();
+        $('div.styledSelect.active').each(function () {
+            $(this).removeClass('active').next('ul.options').hide();
+        });
+        $(this).toggleClass('active').next('ul.options').toggle();
+    });
+
+    // Hides the unordered list when a list item is clicked and updates the styled div to show the selected list item
+    // Updates the select element to have the value of the equivalent option
+    $listItems.click(function (e) {
+        e.stopPropagation();
+        $styledSelect.text($(this).text()).removeClass('active');
+        $this.val($(this).attr('rel'));
+        $list.hide();
+        /* alert($this.val()); Uncomment this for demonstration! */
+    });
+
+    // Hides the unordered list when clicking outside of it
+    $(document).click(function () {
+        $styledSelect.removeClass('active');
+        $list.hide();
+    });
+
+});
